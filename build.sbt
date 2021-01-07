63b8426c4b51d40eacc32e5f9113f099165d5c4b
Juan David Espitia <juandespitia14@gmail.com>
2021-01-07 15:12:35 -0500

    dependencias actualizadas

--- build.sbt
+++ build.sbt
@@ -1,16 +1,13 @@
+import sbt.Keys.libraryDependencies
 name := "Scala-Spark-ETL-CDS"
 
+crossScalaVersions := Seq("2.11.12", "2.12.10")
 version := "0.1"
-
 scalaVersion := "2.11.12"
 
 val sparkVersion = "2.3.1"
-libraryDependencies ++= Seq(
-  "org.apache.spark" %% "spark-core" % sparkVersion % Provided,
-  "org.apache.spark" %% "spark-sql" % sparkVersion % Provided,
-  "org.apache.spark" %% "spark-streaming"% sparkVersion % Provided,
-  "com.databricks" %% "spark-csv" % "1.5.0" % Provided,
-  "com.crealytics" %% "spark-excel" % "0.8.3"
 
-)
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.2.3"
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.4.3"
+libraryDependencies += "org.apache.spark" %% "spark-hive" % "2.4.3" % "provided"
 
--- src/main/scala/com/spark/cds/DataIngest.scala
+++ src/main/scala/com/spark/cds/DataIngest.scala
@@ -8,12 +8,22 @@ object DataIngest
   def main(args: Array[String]): Unit =
   {
     Logger.getLogger("org").setLevel(Level.ERROR)
-    //Logger.getLogger("spark").setLevel(Level.WARN)
+    Logger.getLogger("spark").setLevel(Level.WARN)
     // Creacion de la variablee para punto de partida de la aplicacion
     val spark = SparkSession.builder().appName("DataIngest-CDS").master("local[*]").getOrCreate()
     // Declaramos la variable de tiempo para calcular cuando se demora la ejecucion del artefacto
     val startTimeMillis = System.currentTimeMillis()
     println(startTimeMillis) // Imprimimos el tiempo de inicio en segundos
+
+    /*
+    * Declaramos los parametros -> Rutas
+    */
+
+    path_input = ""
+
+
+
+
 
 
     val endTimeMillis = System.currentTimeMillis()
